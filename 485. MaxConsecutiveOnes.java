73ee194b3e52e135899c3dbb6d782d96df3c7c15
mio <461152465@qq.com>
2018-04-29 11:22:56 +0800

    Simple Array

--- 485. MaxConsecutiveOnes.java
+++ 485. MaxConsecutiveOnes.java
@@ -1,3 +1,6 @@
+//My Function:
+//第一遍遍历，找到所有最大值，加入到ArrayList
+//第二遍遍历，找到ArrayList中最大值
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         ArrayList<Integer> maxList = new ArrayList<Integer>();
@@ -17,7 +20,26 @@ class Solution {
         for(int i=0;i < maxList.size();i++){
             if(maxList.get(i) >= max) max = maxList.get(i);
         }
-        
         return max;
+    }
+}
+//Discussion:
+//只需要遍历一遍，在遍历的同时就可以找到最大值
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCnt = 0;
+        int cnt = 0;
+        if(nums != null){
+            for(int n : nums){
+                if(n == 1){
+                    cnt++;
+                    maxCnt = cnt > maxCnt ? cnt : maxCnt;
+                }
+                else {
+                    cnt = 0;
+                }
+            }
+        }
+        return maxCnt;
     }
 }
