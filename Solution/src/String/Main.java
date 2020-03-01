acb2766c82d7d7bce1483287c4ee78415983262e
mio4 <461152465@qq.com>
2020-03-01 13:04:58 +0800

    update

--- Solution/src/String/Main.java
+++
@@ -1,7 +0,0 @@
-package String;
-
-public class Main {
-	public static void main(String[] args){
-		System.out.println("me");
-	}
-}
---
+++ solutions/src/node/TreeNode.java
@@ -0,0 +1,8 @@
+package node;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int x){val = x;}
+}
